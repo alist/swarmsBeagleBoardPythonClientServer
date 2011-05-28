2b7196493e94ddb86b6900913dfe8ff7e770afc0
root <root@beagleboard.(none)>
2011-05-28 17:53:42 +0000

    python accesses basic cout from driveLib!!

--- pyDriveLink.py
+++ pyDriveLink.py
@@ -1,4 +1,9 @@
 import numpy as N
 import ctypes as C
 _foo = N.ctypeslib.load_library('libDrive', '.')
+_foo.printGreeting.argtypes = None
+_foo.printGreeting.restype = C.c_void_p
+def printGreeting():
+	return _foo.printGreeting();
 
+printGreeting();
